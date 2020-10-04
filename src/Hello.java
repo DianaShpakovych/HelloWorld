81fa41fc380c6c7a769ad438c015c61e2cd25449
DianaShpakovych <dianashpakovych@gmail.com>
2020-10-04 16:53:24 +0200

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int myFirstNumber = 25 + 50;
+        int mySecondNumber = 30;
+        int myThirdNumber = 66;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println("myTotal " + myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println("myLasOne " + myLastOne);
+
+
+    }
+
+
+
+}
